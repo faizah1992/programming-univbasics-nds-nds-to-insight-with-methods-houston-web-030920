1a3ec6eb33f30b18e0707d932c25e667d0ebb1f1
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-27 16:09:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,9 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-Total=0 
-director_index=0 
-while  director_index<director_data.count do 
+ 
   
   
 end
